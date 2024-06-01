0271d7054ccb5fa8905864ecd23fb0e1160bd5ad
dwiyudha0444 <yudhacamo12@gmail.com>
2024-06-01 20:41:32 +0700

    database migrate prediksi

--- app/Http/Controllers/admin/PrediksiController.php
+++ app/Http/Controllers/admin/PrediksiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Produk;
+use App\Models\Prediksi;
 use DB;
 
 class PrediksiController extends Controller
@@ -18,5 +19,11 @@ class PrediksiController extends Controller
     public function test()
     {
         return view('admin.prediksi.test');
+    }
+
+    public function create()
+    {
+        $rel_produk = Prediksi::orderBy('id','DESC')->get();
+        return view('admin.produk.form',compact('rel_produk'));
     }
 }
---
+++ app/Models/Prediksi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prediksi extends Model
+{
+    protected $table = 'prediksis';
+    
+    protected $fillable = [
+        'id_produk',
+        'id_filter_penjualan_perbulan',
+        'nama',
+        'id_kategori',
+        'harga',
+        'qty', 
+    ];
+}
