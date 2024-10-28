813fb1c893786f817d02bc28b498511ddfcb7a23
dwiyudha0444 <yudhacamo12@gmail.com>
2024-10-28 13:46:17 +0700

    mad logic

--- app/Helpers/mse.php
+++ app/Helpers/mse.php
@@ -5,31 +5,22 @@
 namespace App\Helpers;
 
 use App\Helpers\MovingAverage;
+use App\Helpers\mad;
 use App\Models\FilterPenjualanPerbulan;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class mad
 {
-    public static function calculateMAD($productId)
+    public static function calculateMSE($productId)
     {
-        // Ambil data penjualan terbaru per bulan untuk produk tertentu
-        $latestSales = FilterPenjualanPerbulan::where('id_produk', $productId)
-            ->orderBy('created_at', 'desc')
-            ->first();
-
-
-        if (!$latestSales) {
-            return 0;
-        }
-
-        $movingAverage = MovingAverage::calculateMovingAveragePeriodeTiga($productId);
+        $mad = mad::calculateMAD($productId);
 
         // Misalkan $mad adalah hasil dari beberapa perhitungan yang akan Anda lakukan
         // Gantilah logika ini dengan logika perhitungan MAD yang sebenarnya
-        $mad = $latestSales->qty - $movingAverage; // atau beberapa perhitungan lain
+        $mse = $mad * $mad; // atau beberapa perhitungan lain
 
-        return $mad;
+        return $mse;
     }
 
     public static function calculateMADPeriodeEmpat($productId)
